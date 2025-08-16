a908c0f60c19fafcb8e0f74b70edab5878982b13
Ar-Ragul <raghulraghunath3@gmail.com>
2025-08-16 23:53:51 +0530

    SignUp llogin, Forgot Screen and the Ui components

--- dreamshell-ui/src/App.tsx
+++ dreamshell-ui/src/App.tsx
@@ -1,6 +1,30 @@
-import DreamshellTerminal from "./DreamshellTerminal";
+import { useState } from "react";
+import { LoginScreen } from "./components/LoginScreen";
+import { SignUpScreen } from "./components/SignUpScreen";
+import { ForgotPasswordScreen } from "./components/ForgotPasswordScreen";
 import './index.css';
 
+type AuthScreen = "login" | "signup" | "forgot-password";
+
 export default function App() {
-  return <DreamshellTerminal />;
+  const [currentScreen, setCurrentScreen] = useState<AuthScreen>("login");
+
+  const showLogin = () => setCurrentScreen("login");
+  const showSignUp = () => setCurrentScreen("signup");
+  const showForgotPassword = () => setCurrentScreen("forgot-password");
+
+  switch (currentScreen) {
+    case "signup":
+      return <SignUpScreen onBackToLogin={showLogin} />;
+    case "forgot-password":
+      return <ForgotPasswordScreen onBackToLogin={showLogin} />;
+    default:
+      return (
+        <LoginScreen 
+          onForgotPassword={showForgotPassword} 
+          onSignUp={showSignUp} 
+        />
+        
+      );
+  }
 }
